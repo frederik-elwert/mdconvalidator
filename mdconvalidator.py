f4e132d1670a5dec1de2395607addaf84068a58e
Frederik Elwert <frederik.elwert@rub.de>
2019-10-22 10:51:33 +0200

    Create TEI and HTML output and pack as .dhc file.

--- mdconvalidator.py
+++ mdconvalidator.py
@@ -3,6 +3,8 @@
 import sys
 import argparse
 import logging
+import tempfile
+import shutil
 from pathlib import Path
 
 import pypandoc
@@ -10,9 +12,18 @@ from lxml import etree
 
 
 DATAPATH = Path('.')
-TEI_TEMPLATE = DATAPATH / 'template' / 'mdconvalidator_tei.xml'
+TEMPLATES = {
+    'tei': DATAPATH / 'template' / 'mdconvalidator_tei.xml',
+}
 CSL = DATAPATH / 'csl' / 'digital_humanities_abstracts.csl'
-SCHEMA = DATAPATH / 'schema' / 'dhconvalidator.xsd'
+SCHEMAS = {
+    'tei': DATAPATH / 'schema' / 'dhconvalidator.xsd'
+}
+EXT = {
+    'tei': '.xml',
+    'html': '.html',
+}
+# TODO: Validation fails w/o publisher. Make this configurable.
 PUBLISHER = 'Frederik Elwert, Ruhr University Bochum'
 
 
@@ -25,36 +36,65 @@ class MDConvalidator:
     def __init__(self, infile, outfile):
         self.infile = infile
         self.outfile = outfile
+        self.tempdir = tempfile.TemporaryDirectory()
 
-    def convalidate(self):
-        self.convert()
-        self.validate()
+    def _get_file_path(self, ext=None):
+        file_path = Path(self.tempdir.name) / Path(self.outfile).name
+        if ext:
+            file_path = file_path.with_suffix(ext)
+        return file_path
 
-    def convert(self):
+    def convalidate(self, formats=['tei', 'html'], validate=['tei']):
+        # Convert into all given formats
+        outfiles = {}
+        for format_ in formats:
+            outfiles[format_] = self.convert(format_)
+        # Validate given formats
+        for format_ in validate:
+            self.validate(outfiles[format_], format_)
+        # Pack as .dhc
+        archive_tempdir = tempfile.TemporaryDirectory()
+        archive_base = Path(archive_tempdir.name) / Path(self.outfile).stem
+        archive = shutil.make_archive(archive_base,
+                                      'zip',
+                                      self.tempdir.name)
+        shutil.copyfile(archive, self.outfile)
+
+    def convert(self, format_):
+        # Use infile directory as base for image paths and other resources
         basedir = Path(self.infile).parent
+        # Common pipeline
+        pandoc_filters = [
+            'pandoc-citeproc',
+        ]
         pandoc_args = [
-            f'--template={TEI_TEMPLATE}',
+            '--standalone',
             f'--csl={CSL}',
             f'--variable=publisher:"{PUBLISHER}"',
             f'--resource-path={basedir}',
         ]
-        pandoc_filters = [
-            'pandoc-citeproc',
-        ]
+        # Check if a custom template is configured
+        if format_ in TEMPLATES:
+            template = TEMPLATES[format_]
+            pandoc_args.append(f'--template={template}')
+        # Get file name
+        outfile = self._get_file_path(EXT[format_])
+        # Do the conversion
         output = pypandoc.convert_file(self.infile,
-                                       to='tei',
+                                       to=format_,
                                        extra_args=pandoc_args,
                                        filters=pandoc_filters,
-                                       outputfile=self.outfile)
-        logging.info(f'Converted {self.infile} to {self.outfile}.')
+                                       outputfile=str(outfile))
+        logging.info(f'Converted {self.infile} to {outfile}.')
+        return outfile
 
-    def validate(self):
-        doctree = etree.parse(self.outfile)
-        schematree = etree.parse(str(SCHEMA))
+    def validate(self, file_, format_):
+        doctree = etree.parse(str(file_))
+        schematree = etree.parse(str(SCHEMAS[format_]))
         schema = etree.XMLSchema(schematree)
 
         schema.assertValid(doctree)
-        logging.info('Generated TEI document is valid.')
+        logging.info(f'Generated {format_} document is valid.')
 
 
 def main():
